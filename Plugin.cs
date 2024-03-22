3cbb98c6360b773c577c821b22a16cab112e1a8e
Max Knudsen <maxknudsen501@gmail.com>
2024-03-22 17:26:03 -0400

    Added initial code and files
    - Added initial plugin code
    - Updated manifest.json with correct name, description, and link
    - Changed Template.csproj to GiantOverhaul.csproj and updated assembly name and description
    - Updated README file

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,17 @@
 ﻿using BepInEx;
-namespace Template;
+using HarmonyLib;
+namespace GiantOverhaul;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
     private void Awake()
     {
         // Plugin load logic goes here!
         // This script acts like a unity object.
-        Logger.LogInfo($"Hello World!");
+        harmony.PatchAll();
+        Logger.LogInfo($"Giant Overhaul Active!");
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# LCOS-ModTemplate
+# LCOS-GiantOverhaul
+
+This mod changes the giant AI to work with the Candyland map (created by LCOS)
+
+The specific AI changes are as follows:
+    - Makes the giant passive during the day
